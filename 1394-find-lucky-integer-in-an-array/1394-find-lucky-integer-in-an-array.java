ceb223ccf0a011e03d786ed65d05c134b55a6a8d
Ketan Nandakumar Patil <ketanpatil9907@gmail.com>
2026-08-10 23:52:20 +0530

    Time: 5 ms (74.01%), Space: 45.3 MB (37.69%) - LeetHub

---
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class Solution {
+    public int findLucky(int[] arr) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count the frequency of each number
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int ans = -1;
+
+        // Check each number and its frequency
+        for (int key : map.keySet()) {
+            if (key == map.get(key)) {
+                ans = Math.max(ans, key);
+            }
+        }
+
+        return ans;
+    }
+}
